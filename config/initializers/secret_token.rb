9bfe59d47412f7acb66784882e72403205fb5795
jsalig1 <jeremy.salig@gmail.com>
2014-07-21 15:58:52 -0400

    generate rails framework

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Thiscloudlookslike::Application.config.secret_key_base = '50d37121af7538191b4c344b5fe0d332512915464352e0482454b6fc528508d53104fa963e84d42ed87701a70b2c6b4b27eaa59f1e7f10cdd1f7e659fa1fe4e5'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Thiscloudlookslike::Application.config.session_store :cookie_store, key: '_thiscloudlookslike_session'
